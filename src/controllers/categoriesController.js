12c66850b8d3cb6d78e404246ac3927e8e19d693
André Zopolato <andrezopo@gmail.com>
2022-07-21 22:07:23 -0300

    cria CR da rota de categorias

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,9 +1,40 @@
 import connection from "../dbStrategy/postgres.js";
 
 export async function getCategories(req, res) {
-  res.send("sou a rota de receber categorias");
+  try {
+    const { rows: categories } = await connection.query(`
+    SELECT * FROM categories
+    `);
+
+    if (categories.length === 0) {
+      res.status(404).send("No categories yet!");
+      return;
+    }
+
+    res.status(200).send(categories);
+  } catch (err) {
+    res.status(500).send("Internal Error");
+  }
 }
 
 export async function createCategory(req, res) {
-  res.send("sou a rota de criar uma categoria");
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      res.status(422).send("Unprocessable entity! Review your request!");
+      return;
+    }
+
+    await connection.query(
+      `
+    INSERT INTO categories (name) VALUES ('$1')
+    `,
+      [name]
+    );
+
+    res.status(201).send("Created");
+  } catch (err) {
+    res.status(500).send("Internal Error");
+  }
 }
