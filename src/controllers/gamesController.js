1ffecc28066988e86455838ff06c6b3ccf655fc3
André Zopolato <andrezopo@gmail.com>
2022-07-21 22:22:22 -0300

    adiciona CR da rota de games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,9 +1,40 @@
 import connection from "../dbStrategy/postgres.js";
 
 export async function getGames(req, res) {
-  res.send("sou a rota de receber jogos");
+  try {
+    const { rows: games } = await connection.query(`
+    SELECT * FROM games
+    `);
+
+    if (games.length === 0) {
+      res.status(404).send("No games yet!");
+      return;
+    }
+
+    res.status(200).send(games);
+  } catch (err) {
+    res.status(500).send("Internal Error!");
+  }
 }
 
 export async function createGame(req, res) {
-  res.send("sou a rota de criar um jogo");
+  try {
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+    if (!name || !image || !stockTotal || !categoryId || !pricePerDay) {
+      res.status(422).send("Unprocessable entity! Review your request!");
+      return;
+    }
+
+    await connection.query(
+      `
+      INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ('$1', '$2', $3, $4, $5)
+    `,
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+
+    res.send("sou a rota de criar um jogo");
+  } catch (err) {
+    res.status(500).send("Internal Error!");
+  }
 }
