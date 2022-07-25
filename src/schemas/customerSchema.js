0f2549373d89defd8c6855a046467be5c09335f3
André Zopolato <andrezopo@gmail.com>
2022-07-25 11:26:20 -0300

    aprimora validação de data de nascimento do cliente

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -1,4 +1,7 @@
 import joi from "joi";
+import dayjs from "dayjs";
+
+const dateNow = dayjs().format("YYYY-MM-DD");
 
 const regex = /[0-9]{11}/;
 const phoneRegex = /[0-9]{10,11}/;
@@ -7,7 +10,7 @@ const customerSchema = joi.object({
   name: joi.string().min(1).required(),
   cpf: joi.string().regex(regex).required(),
   phone: joi.string().regex(phoneRegex).required(),
-  birthday: joi.date().required(),
+  birthday: joi.date().max(dateNow).required(),
 });
 
 export default customerSchema;
